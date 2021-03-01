d1e8f69caf63dcf2050b0dccd763468e8c841bf9
Chiri Iridia Vulpes <yuudaari@gmail.com>
2021-03-01 22:07:59 +1100

    Watch client/*.ts and utilities/*.ts for dev build as well

--- tasks/watch.ts
+++ tasks/watch.ts
@@ -6,6 +6,6 @@ import Task, { TaskFunction } from "./utilities/Task";
 const rebuild: TaskFunction<Promise<void>> = task => task.series(clean, build);
 
 export default Task("watch", task =>
-	chokidar.watch(["site/**/*.ts", "style/**/*.scss", "static/**/*"], { ignoreInitial: true })
+	chokidar.watch(["site/**/*.ts", "style/**/*.scss", "static/**/*", "client/**/*.ts", "utilities/**/*.ts"], { ignoreInitial: true })
 		.on("all", () =>
 			task.debounce(rebuild)));
