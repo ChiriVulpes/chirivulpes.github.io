17c875bc9f7746e089679029eb3f43dd7bf95254
Chiri Iridia Vulpes <yuudaari@gmail.com>
2021-03-02 23:40:50 +1100

    Remove unnecessary fragments

--- site/page/index/MusicArticle.ts
+++ site/page/index/MusicArticle.ts
@@ -1,5 +1,4 @@
 import Article from "@element/Article";
-import { Fragment } from "@element/Element";
 import Iframe from "@element/Iframe";
 
 export default new Article("Music")
@@ -8,11 +7,10 @@ export default new Article("Music")
 		.setNav(nav => nav
 			.link("SoundCloud")
 			.link("Bandcamp"))
-		.append(new Fragment()
-			.markdown(`
-				I used to make music a lot, but not too much anymore. It never really clicked for me like other things that I do, it was always a struggle to make anything. I could never produce the sound I wanted to, even if I knew exactly what I wanted, and that just made it a struggle. Maybe someday I'll pick it up again and figure things out further.
+		.markdown(`
+			I used to make music a lot, but not too much anymore. It never really clicked for me like other things that I do, it was always a struggle to make anything. I could never produce the sound I wanted to, even if I knew exactly what I wanted, and that just made it a struggle. Maybe someday I'll pick it up again and figure things out further.
 
-				For what it's worth, here's the song I'm proudest of:`))
+			For what it's worth, here's the song I'm proudest of:`)
 		.append(new Iframe("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/661307534&color=%23387eff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true")
 			.setTitle("Moore's Law — Chirichirichiri")
 			.setHeight(166)));
--- site/page/index/StoriesArticle.ts
+++ site/page/index/StoriesArticle.ts
@@ -1,5 +1,4 @@
 import Article from "@element/Article";
-import { Fragment } from "@element/Element";
 import Story from "site/collections/stories/Story";
 
 export default new Article<Story>("Stories")
@@ -8,9 +7,8 @@ export default new Article<Story>("Stories")
 			.link("Patreon")
 			.link("itch.io")
 			.link("Scribble Hub"))
-		.append(new Fragment()
-			.markdown(`
-				I write transgender-themed novels! I work in all sorts of genres from realistic fiction to fantasy to sci-fi and more. If you're trans or you like to read stories about people coming to understand aspects of themselves, and getting happier all the while, consider checking them out!
+		.markdown(`
+			I write transgender-themed novels! I work in all sorts of genres from realistic fiction to fantasy to sci-fi and more. If you're trans or you like to read stories about people coming to understand aspects of themselves, and getting happier all the while, consider checking them out!
 
-				**I take commissions!** If you'd like to commission a chapter, check out my [Patreon].`)))
+			**I take commissions!** If you'd like to commission a chapter, check out my [Patreon].`))
 	.setContainsCards(Story, "site/collections/stories", "status");
