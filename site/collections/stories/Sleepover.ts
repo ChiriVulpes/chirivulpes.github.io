e761533fb060932592076c2202e7aff802ecf295
Chiri Vulpes <yuudaari@gmail.com>
2021-09-08 14:15:17 +1000

    Fix sleepover having the wrong wordcount

--- site/collections/stories/Sleepover.ts
+++ site/collections/stories/Sleepover.ts
@@ -3,9 +3,9 @@ import Story from "site/collections/stories/Story";
 export default new Story("A Very Girly Sleepover to Which I Was Not Invited")
 	.setCover("sleepover")
 	.setStatus("Completed")
-	.setLength(4000, 1)
+	.setLength(5000)
 	.setLink("https://www.scribblehub.com/series/285519/a-very-girly-sleepover-to-which-i-was-not-invited/")
-	.setDate("2021-01-03T13:00:00.000Z")
+	.setDate("2021-05-11T13:00:00.000Z")
 	.setOrder(100)
 	.setSynopsis(`
 		I hated sleepovers! Whenever my sister had them it made me want to scream, hearing the talking and giggles, the house being filled with girls and stuffed animals and cute clothes.
