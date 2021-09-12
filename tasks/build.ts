4a1f4f82ba2a7dc69f37c7ef7e9fb6d1db224c12
Chiri Vulpes <yuudaari@gmail.com>
2021-09-12 18:45:55 +1000

    Try no extension for ts-node thru github actions?

--- tasks/build.ts
+++ tasks/build.ts
@@ -3,7 +3,9 @@ import path from "path";
 import Task from "./utilities/Task";
 
 export default Task("build", () => new Promise<void>((resolve, reject) => {
-	const childProcess = spawn(path.resolve("node_modules/.bin/ts-node.cmd"), ["-r", "tsconfig-paths/register", "site/main.ts"],
+	console.log(process.platform);
+	const ext = process.platform === "win32" ? ".cmd" : "";
+	const childProcess = spawn(path.resolve("node_modules/.bin/ts-node" + ext), ["-r", "tsconfig-paths/register", "site/main.ts"],
 		{ stdio: [process.stdin, process.stdout, process.stderr] });
 	childProcess.on("error", reject);
 	childProcess.on("exit", code => {
