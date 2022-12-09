5073b5cb1cd30aac0d246a151737964d7eebc44d
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2022-12-09 14:52:09 +1300

    fvm -> deepsight.gg

--- site/collections/projects/deepsight.gg.ts
+++ site/collections/projects/deepsight.gg.ts
@@ -1,16 +1,16 @@
 import Element from "@element/Element";
 import Project from "site/collections/projects/Project";
 
-export default new Project("fvm.chiri.works")
+export default new Project("deepsight.gg")
 	.setOrder(2)
 	.setType("Websites")
-	.setImage("https://fvm.chiri.works/image/logo.png", image => image
+	.setImage("https://deepsight.gg/image/logo.png", image => image
 		.class("borderless"))
-	.setLink("https://fvm.chiri.works")
+	.setLink("https://deepsight.gg")
 	.setDetails(details => details
 		.append(new Element("span").class("date").text("2022. "))
 		.text("A vault manager for Destiny 2, boldly stepping in directions that other vault managers don't."))
 	.setDescription(`
 		An entirely static web app using entirely client-side TypeScript, hosted on GitHub pages. The only dependency is one that provides some types for interacting with the Bungie API. Pretty experimental in the way it's structured and rendered.
 		
-		fvm is a long-term project for me; it's a blank slate for me to add more features and polish to until it's to the point where it's silly for people *not* to use it. And until then, something for me to chip away at whenever I'm in the mood.`);
+		deepsight.gg is a long-term project for me; it's a blank slate for me to add more features and polish to until it's to the point where it's silly for people *not* to use it. And until then, something for me to chip away at whenever I'm in the mood.`);
